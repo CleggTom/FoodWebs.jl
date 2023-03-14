ec27f971514a96c78a39403d336be98edf7310b3
Tom <t.clegg17@imperial.ac.uk>
2023-03-14 10:22:48 +0100

    inital commit

---
+++ src/generalisedjacobian.jl
@@ -0,0 +1,118 @@
+
+"""
+    GeneralisedParameters
+
+struct to contain the parameters for the normalised model. Includes the
+"""
+struct GeneralisedParameters
+    #system parameters 
+    N::Int64
+    n::Vector{Float64}
+    A::Matrix{Float64}
+
+    #scale parameters
+    α::Vector{Float64}
+    β::Matrix{Float64}
+    χ::Matrix{Float64}
+    ρ::Vector{Float64}
+    ρ̃::Vector{Float64}
+    σ::Vector{Float64}
+    σ̃::Vector{Float64}
+
+    #exponent parameters
+    γ::Vector{Float64}
+    λ::Matrix{Float64}
+    μ::Vector{Float64}
+    ϕ::Vector{Float64}
+    ψ::Vector{Float64}
+end
+
+
+"""
+    generalised_paramers(A,n)
+
+Randomly samples a set of generalised parameters from a given interaction matrix and set of niches 
+"""
+function generalised_paramers(A,n)
+    N = size(A)[1]
+    #scale
+    α = n .^ (-0.25)
+
+    β = zeros(N,N)
+    χ = zeros(N,N)
+    for i = 1:N
+        for j = 1:N
+            #relative loss of j due to predation by i
+            β[i,j] = A[i,j] / sum(A[:,j]) 
+            #relative contributuion of j to total predation by i
+            χ[i,j] = A[i,j] / sum(A[i,:]) 
+        end
+    end
+
+    β[isnan.(β)] .= 0.0
+    χ[isnan.(χ)] .= 0.0
+
+    ρ = 1 .* (sum(A,dims=2) .!= 0)[:]
+    ρ̃ = 1 .- ρ
+    
+    σ = 1 .* (sum(A,dims=1) .== 0)[:]
+    σ̃ = 1 .- σ
+
+    #exponent
+    γ = rand(N) .+ 0.5 #[0.5,1.5]
+    λ = ones(N,N)
+    μ = ones(N) .* 2#rand(N) .+ 1.0 #[1.0,2.0]
+    ϕ = rand(N)
+    ψ = rand(N) .+ 0.5
+
+    return GeneralisedParameters(N,n,A,α,β,χ,ρ,ρ̃,σ,σ̃,γ,λ,μ,ϕ,ψ)
+end
+
+
+"""
+    generalised_paramers(com::Community)
+
+generate parameters directly from a Community object
+"""
+function generalised_paramers(com::Community)
+    generalised_paramers(com.A, [s.n for s = com.sp])
+end
+"""
+    generalised_jacobian!(J,p::GeneralisedParameters)
+
+Constructs the jaccobian in place as defined by the generalised model. Takes parameter values (including network structure) from the `GeneralisedParameters` object `p`.
+"""
+function generalised_jacobian!(J,p::GeneralisedParameters)
+    #construct generalised_jacobian
+    for i = 1:p.N
+        for j = 1:p.N
+            if i == j #intra
+                J[i,j] = p.ρ̃[i] * p.ϕ[i] + #production
+                         p.ρ[i] * (p.γ[i] * p.χ[i,j] * p.λ[i,j] + p.ψ[i]) - #predation
+                         p.σ̃[i] * p.μ[i] #mortality
+                for k = 1:p.N
+                    J[i,j] -= p.σ[i] * p.β[k,i] * p.λ[k,i] * ((p.γ[k] - 1) * p.χ[k,i] + 1) #predation
+                end
+            else #inter
+                J[i,j] = p.ρ[i] * p.γ[i] * p.χ[i,j] * p.λ[i,j] - #predation
+                         p.σ[i] * p.β[j,i] * p.ψ[j]
+                for k = 1:p.N
+                    J[i,j] -= p.σ[i] * (p.β[k,i] * p.λ[k,j] * (p.γ[k] - 1) * p.χ[k,j])
+                end
+            end
+            J[i,j] *= p.α[i]
+        end
+    end
+    
+end
+
+"""
+    generalised_jacobian(p::GeneralisedParameters)
+
+generate jacobian from parameter set p
+"""
+function generalised_jacobian(p::GeneralisedParameters)
+    J = zeros(p.N, p.N)
+    generalised_jacobian!(J,p)
+    return(J)
+end
---
+++ src/nichemodel.jl
@@ -0,0 +1,95 @@
+using Distributions
+using CairoMakie
+using UUIDs
+using LinearAlgebra
+
+
+"""
+    Sp
+
+type to store niche model parameters for a single species, its thermal optima and a unique ID.  
+"""
+struct Sp
+    n::Float64
+    r::Float64
+    c::Float64
+    Tpk::Float64
+    id::UUID
+end
+
+"""
+    Community
+
+type containing parameters defining a community including its interaction matrix and a vector of species within.
+"""
+struct Community
+    A::Matrix{Float64}
+    sp::Vector{Sp}
+end
+
+"""
+    sp(C::Float64)
+
+Constructor function for Sp type. Randomly generates niche parameters given a connectance value
+"""
+function Sp(C::Float64)
+    β = (1 / (2*C)) - 1
+    n = rand(Uniform(0,1))
+    r = n * rand(Beta(1.0, β))
+    c = rand(Uniform(r / 2, n))
+    Tpk = rand()
+    uuid = UUIDs.uuid1()
+    return(Sp(n,r,c,Tpk,uuid))
+end
+
+
+"""
+    web(sp_vec::Vector{Sp})
+
+Generates an adjacency matrix for a given set of species using the niche model. 
+"""
+function web(sp_vec::Vector{Sp})
+    N = length(sp_vec)
+    A = zeros(N,N)
+
+    for (i,sp_i) = enumerate(sp_vec)
+        for (j,sp_j) = enumerate(sp_vec)
+            if sp_j.n < (sp_i.c + (sp_i.r/2))
+                if  sp_j.n > (sp_i.c - (sp_i.r/2))
+                    A[i,j] = 1
+                end
+            end
+        end
+    end
+
+    return(Community(A, sp_vec))
+end
+
+"""
+    web(sp_vec::Vector{Sp}, N::Int64, T::Float64, T_range::Float64)
+
+Randomly assembles a food web from a set of species of size N at temperature T. Species are selected within a range of T_range. Returns adjacency matrix. 
+"""
+function web(sp_vec::Vector{Sp}, N::Int64, T::Float64, T_range::Float64)
+    #select sp based on Temp range...
+    indx = findall([s.Tpk < T + T_range && s.Tpk > T - T_range for s = sp_vec])
+    
+    N_T = min(length(indx),N)
+
+    #sample
+    sp_vec_indx = sp_vec[sortperm(rand(length(indx)))[1:N_T]]
+
+    community = web(sp_vec_indx)
+
+    return(community)
+end
+
+"""
+    web(N,C)
+
+Constructs a food web of size N and connectance C by randomly generating species. This function will automatically remove isolated species and add new ones. 
+"""
+
+
+
+
