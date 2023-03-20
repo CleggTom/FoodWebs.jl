9d5562dab0be996610952428b951ddf3d662725b
Tom <t.clegg17@imperial.ac.uk>
2023-03-20 14:58:25 +0100

    Update generalised modeling parameters

--- src/generalisedjacobian.jl
+++ src/generalisedjacobian.jl
@@ -2,7 +2,7 @@
 """
     GeneralisedParameters
 
-struct to contain the parameters for the normalised model. Includes the
+Type to contain the parameters for the normalised model.
 """
 struct GeneralisedParameters
     #system parameters 
@@ -29,11 +29,11 @@ end
 
 
 """
-    generalised_paramers(A,n)
+    generalised_parameters(A,n)
 
 Randomly samples a set of generalised parameters from a given interaction matrix and set of niches 
 """
-function generalised_paramers(A,n)
+function generalised_parameters(A,n)
     N = size(A)[1]
     #scale
     α = n .^ (-0.25)
@@ -74,15 +74,18 @@ end
 
 generate parameters directly from a Community object
 """
-function generalised_paramers(com::Community)
+function generalised_parameters(com::Community)
     generalised_paramers(com.A, [s.n for s = com.sp])
 end
+
+
 """
     generalised_jacobian!(J,p::GeneralisedParameters)
 
 Constructs the jaccobian in place as defined by the generalised model. Takes parameter values (including network structure) from the `GeneralisedParameters` object `p`.
 """
 function generalised_jacobian!(J,p::GeneralisedParameters)
+    @assert size(J) == size(p.A)
     #construct generalised_jacobian
     for i = 1:p.N
         for j = 1:p.N
@@ -114,5 +117,86 @@ generate jacobian from parameter set p
 function generalised_jacobian(p::GeneralisedParameters)
     J = zeros(p.N, p.N)
     generalised_jacobian!(J,p)
-    return(J)
+    return J 
+end
+
+"""
+    max_real_eigval(J)
+
+Get the real part of the maximum eigenvalue from J allowing for complex values. 
+"""
+function max_real_eigval(J)
+    λ = eigvals(J)[end]
+    if isa(λ, Complex)
+        return(λ.re)
+    else
+        return(λ)
+    end
+end
+
+"""
+    communtiy_stability(J,p::GeneralisedParameters)
+
+Get the real part of the leading eigenvalue from parameter set p. J is provided to avoid allocation
+"""
+function communtiy_stability(J,p::GeneralisedParameters)
+    generalised_jacobian!(J,p::GeneralisedParameters)
+    return max_real_eigval(J) 
+end
+
+"""
+    communtiy_stability(p::GeneralisedParameters)
+
+Non-inplace version.
+"""
+function communtiy_stability(p::GeneralisedParameters)
+    J = zeros(size(p.A))
+    return communtiy_stability!(J,p::GeneralisedParameters)
+end
+
+"""
+    communtiy_stability(J, c::Community)
+
+Calculates real part of the leading eigenvalue directly from a Communtiy object. 
+"""
+function communtiy_stability(J, c::Community)
+    #make params
+    p = generalised_parameters(c)
+    return communtiy_stability(J, p)
+end
+
+"""
+    communtiy_stability(c::Community)
+
+Calculates real part of the leading eigenvalue directly from a Communtiy object. 
+"""
+function communtiy_stability(c::Community)
+    #make params
+    J = zeros(size(p.A))
+    p = generalised_parameters(c)
+    return communtiy_stability!(J, p)
+end
+
+
+function proportion_stable_webs(J,c::Community, N::Int = 100)
+    sum([communtiy_stability(J, c) for i = 1:N] .< 0) / N
+end
+
+"""
+    proportion_stable_webs(c::Community, N::Int = 100)
+
+Calculates the proportion stable parameter configurations for a Community.   
+"""
+function proportion_stable_webs(c::Community, N::Int = 100)
+    J = zeros(size(c.A))
+    sum([communtiy_stability(J, c) for i = 1:N] .< 0) / N
+end
+ 
+function proportion_stable_webs(mc::MetaCommunity,N::Int = 100)
+    props = zeros(size(mc.coms))
+    for (i,c) = enumerate(mc.coms)
+        J = zeros(size(c.A))
+        props[i] = proportion_stable_webs(J,c,N)
+    end
+    return props 
 end
